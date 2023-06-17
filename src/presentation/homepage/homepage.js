e2d7f72d73febafd71bd7cb9649568a11dec295f
Naman Anand <voidmemories@Namans-MacBook-Air.local>
2023-06-18 00:41:35 +0530

    implement loading animation

--- src/presentation/homepage/homepage.js
+++ src/presentation/homepage/homepage.js
@@ -1,10 +1,37 @@
 import Lottie from "lottie-react";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import breathingShape from "../../assets/animations/breathing-shape.json";
 import pulse from "../../assets/animations/pulsating.json";
 
 function Homepage() {
   const [triggerAnimation, setTriggerAnimation] = useState(false);
+  const [triggerAnimation2, setTriggerAnimation2] = useState(false);
+  const [triggerAnimation3, setTriggerAnimation3] = useState(false);
+  const [triggerAnimation4, setTriggerAnimation4] = useState(false);
+
+  useEffect(() => {
+    if (triggerAnimation3) {
+      var body = document.querySelector("body"),
+        bar = document.querySelector(".progress-bar"),
+        counter = document.querySelector(".count"),
+        i = 0;
+
+      (function draw() {
+        if (i <= 100) {
+          requestAnimationFrame(draw);
+          bar.style.width = i + "%";
+          counter.innerHTML = Math.round(i) + "%";
+
+          i = i + 0.3;
+        } else {
+          bar.className += " done";
+          setTimeout(() => {
+            setTriggerAnimation4(true);
+          }, 300);
+        }
+      })();
+    }
+  }, [triggerAnimation3]);
 
   return (
     <div className="bg-slate-800 w-screen h-screen">
@@ -65,12 +92,17 @@ function Homepage() {
 
       {triggerAnimation && (
         <section
-          className={
-            "section-3 bg-backgroundColor h-screen w-screen absolute top-0 left-0 "
-          }
+          className={`${
+            triggerAnimation2 ? "section-1" : "section-3"
+          } bg-backgroundColor h-screen w-screen absolute top-0 left-0`}
         >
           <div className="top-1/2 -translate-y-1/2 left-1/2 -translate-x-1/2 relative grid gap-2 grid-cols-2 grid-rows-2">
-            <div className="choice-cell container w-1/2 relative max-w-xs rounded-2xl justify-self-end">
+            <div
+              className="choice-cell container w-1/2 relative max-w-xs rounded-2xl justify-self-end"
+              onClick={() => {
+                setTriggerAnimation2(true);
+              }}
+            >
               <img
                 src="/assets/images/ecom-usecase.jpg"
                 className="w-full h-full rounded-2xl choice-cell-img"
@@ -122,6 +154,102 @@ function Homepage() {
               </div>
             </div>
           </div>
+        </section>
+      )}
+
+      {triggerAnimation2 && (
+        <section
+          className={
+            "section-2 w-screen h-screen bg-red-500 z-50 absolute top-0 left-0 rounded-3xl"
+          }
+        >
+          <p className="text-6xl filler-title-local text-center m-40">
+            <span>select the ML model to use</span>
+          </p>
+        </section>
+      )}
+
+      {triggerAnimation2 && (
+        <section
+          className={`${
+            triggerAnimation3 ? "section-1" : "section-3"
+          } bg-backgroundColor h-screen w-screen absolute top-0 left-0`}
+        >
+          <div className="top-1/2 -translate-y-1/2 left-1/2 -translate-x-1/2 relative grid gap-2 grid-cols-2 grid-rows-2">
+            <div
+              className="choice-cell container w-1/2 relative max-w-xs rounded-2xl justify-self-end"
+              onClick={() => setTriggerAnimation3(true)}
+            >
+              <img
+                src="/assets/images/models/ml-model-1.jpg"
+                className="w-full h-full rounded-2xl choice-cell-img"
+              />
+              <div className="choice-cell-hidden">
+                <p className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 text-3xl text-white font-medium z-10">
+                  Checkout
+                  <br />
+                  Recommendation
+                </p>
+                <div className="bg-black opacity-80 w-full h-full absolute rounded-2xl top-0 left-0" />
+              </div>
+            </div>
+
+            <div className="choice-cell container w-1/2 relative max-w-xs rounded-2xl justify-self-start">
+              <img
+                src="/assets/images/models/ml-model-2.jpg"
+                className="w-full h-full rounded-2xl choice-cell-img"
+              />
+              <div className="choice-cell-hidden">
+                <p className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 text-3xl text-white font-medium z-10">
+                  Discount
+                  <br />
+                  Matcher
+                </p>
+                <div className="bg-black opacity-80 w-full h-full absolute rounded-2xl top-0 left-0" />
+              </div>
+            </div>
+
+            <div className="choice-cell container w-1/2 relative max-w-xs rounded-2xl justify-self-end">
+              <img
+                src="/assets/images/models/ml-model-3.jpg"
+                className="w-full h-full rounded-2xl choice-cell-img"
+              />
+              <div className="choice-cell-hidden">
+                <p className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 text-3xl text-white font-medium z-10">
+                  Route
+                  <br />
+                  Picker
+                </p>
+                <div className="bg-black opacity-80 w-full h-full absolute rounded-2xl top-0 left-0" />
+              </div>
+            </div>
+
+            <div className="choice-cell container w-1/2 relative max-w-xs rounded-2xl justify-self-start">
+              <img
+                src="/assets/images/models/ml-model-4.jpg"
+                className="w-full h-full rounded-2xl choice-cell-img"
+              />
+              <div className="choice-cell-hidden">
+                <p className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 text-3xl text-white font-semibold z-10">
+                  Fraud
+                  <br />
+                  Detection
+                </p>
+                <div className="bg-black opacity-80 w-full h-full absolute rounded-2xl top-0 left-0" />
+              </div>
+            </div>
+          </div>
+        </section>
+      )}
+
+      {triggerAnimation3 && (
+        <section
+          className={`${
+            triggerAnimation4 ? "expand-to-emulator-screen" : "section-4"
+          } bg-backgroundColor h-screen w-screen absolute top-0 left-0`}
+        >
+          <div className="progress-bar z-50"></div>
+          <h1 className="count"></h1>
         </section>
       )}
     </div>
